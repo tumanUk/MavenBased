d7acd589b235f117bf56ce37f6731dc5188a2121
dmytro.hrynevych <dmytro.hrynevych@hermes-europe.co.uk>
2022-02-11 20:40:54 +0200

    initial commit

---
+++ src/test/java/ts/SimpleTest.java
@@ -0,0 +1,12 @@
+package ts;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void sumTest() {
+        Assert.assertTrue("Test failed ", true);
+    }
+}
